38c8d7d8a6b7810ac19056ce54e742b4b78106d8
JonathanBollaert <159426039+JonathanBollaert@users.noreply.github.com>
2024-03-07 11:08:19 +0100

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/OrganizationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class OrganizationsController : Controller
+    {
+        private readonly PeopleManagerDbContext _dbContext;
+
+        public OrganizationsController(PeopleManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var organizations = _dbContext.Organizations.ToList();
+            return View(organizations);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Organization organization)
+        {
+            _dbContext.Organizations.Add(organization);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
